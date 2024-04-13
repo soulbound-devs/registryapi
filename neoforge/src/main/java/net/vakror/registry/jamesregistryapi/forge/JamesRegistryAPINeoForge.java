cb059436e6ccc1988637862338717166d38bf08f
vaylor27 <godisagodlol@gmail.com>
2024-04-13 00:27:20 -0400

    fix

--- neoforge/src/main/java/net/vakror/registry/jamesregistryapi/forge/JamesRegistryAPINeoForge.java
+++ neoforge/src/main/java/net/vakror/registry/jamesregistryapi/forge/JamesRegistryAPINeoForge.java
@@ -1,11 +1,13 @@
 package net.vakror.registry.jamesregistryapi.forge;
 
+import dev.architectury.event.EventHandler;
 import net.neoforged.fml.common.Mod;
 import net.vakror.registry.jamesregistryapi.JamesRegistryAPI;
 
 @Mod(JamesRegistryAPI.MOD_ID)
-public class JamesRegistryAPIForge {
-    public JamesRegistryAPIForge() {
+public class JamesRegistryAPINeoForge {
+    public JamesRegistryAPINeoForge() {
+        EventHandler.init();
 		// Submit our event bus to let architectury register our content on the right time
         JamesRegistryAPI.init();
     }
